68456acc76b27ebecb97eaacd8ff37b7da576ce9
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2023-12-21 21:43:14 -0300

    Refactoring das pastas e e exercícios da aula 2.

---
+++ 02-estruturas-condicionais/aula.js
@@ -0,0 +1,10 @@
+const camisetaFelipeEAzul = true;
+const camisetaIsabelleEAzul = false;
+
+let numero = 10;
+let eNumeroPar = (num) => (num % 2 === 0);
+
+console.log(`O número ${numero} é par? ${eNumeroPar(numero) ? 'Sim' : 'Não'}`);
+
+numero = 47847;
+console.log(`O número ${numero} é par? ${eNumeroPar(numero) ? 'Sim' : 'Não'}`);
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Curso Primeiros passos com JavaScript
+
 Repositório para armazenar o conteúdo estudado no curso de JavaScript da plataforma DIO.
 
 ## VARIÁVEIS E OPERADORES
